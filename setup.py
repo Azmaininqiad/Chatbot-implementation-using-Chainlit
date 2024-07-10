671e2cab3349dc86971dd68317118ce437dca85d
Azmaininqiad <63859070+Azmaininqiad@users.noreply.github.com>
2024-07-10 10:02:43 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "FoodPanda Chatbot",
+    version= "0.0.0",
+    author= "Azmain Adeeb",
+    author_email= "azmainadeeb@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
